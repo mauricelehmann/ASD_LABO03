43b3937867bf24f385232710dea96152e5a793b1
spirits3 <flo_diablo@hotmail.com>
2019-04-04 17:28:31 +0200

    tout les test en 1 fichier

--- Labo03.cpp
+++ Labo03.cpp
@@ -23,15 +23,93 @@
 #include <vector>
 #include <string>
 #include <random>
-#include <stdlib.h>
+#include <cstdlib>
 #include <ctime>
 #include <chrono>
-
-#include "countingSort.h"
+#include <type_traits>
+#include <algorithm>
 
 using namespace std;
 using namespace chrono;
-using namespace asd1;
+
+  /**
+   Tri comptage générique
+
+   https://en.wikipedia.org/wiki/Counting_sort
+
+   @param first  [first,last] est la plage d'éléments d'un tableau à trier.
+   @param last   [first,last] est la plage d'éléments d'un tableau à trier.
+   @param output début du tableau où écrire le résultat. Doit être différent
+                 du tableau d'entrée
+   @param key fonction prenant un élément en entrée et retourant sa position
+              dans le tableau de comptage
+   @param max_key valeur maximale pouvant être retournée par key(...). Si -1,
+                  la fonction calcule cette valeur en parcourant le tableau une
+                  fois de plus
+   */
+  template<typename RandomAccessIterator, typename Fn>
+  void CountingSort(RandomAccessIterator first,
+                  RandomAccessIterator last,
+                  RandomAccessIterator output,
+                  Fn key,
+                  size_t max_key = -1)
+  {
+    //La valeur maximume de l'input n'a pas été définie, on doit le chercher
+    if(max_key == -1){
+        max_key = 0;
+        for(auto i = first ; i != last ; i++){
+            if(key(*i) > max_key ){
+                max_key = key(*i);
+            }
+        }
+    }
+    //On initialise un tableau de comptage, de taille égal à max_key
+    //Toute les éléments sont initialisées à zéro
+    std::vector<std::size_t> countingTab(max_key+1,0);
+    //On compte les éléments du vecteur à trier
+    for(auto i = first ; i != last ; i++ ){
+        ++countingTab.at(key(*i));
+    }
+    //On additione chaques éléments avec le précedent
+    for(auto i = countingTab.begin() + 1; i != countingTab.end(); i++){
+        *i += *(i-1);
+    }
+
+    //shift tout les éléments de 1 à droite
+    std::rotate(countingTab.begin(),countingTab.end()-1,countingTab.end());
+    //le premier élément doit être à zéro
+    countingTab.front() = 0;
+
+    //Pour chaque éléments du tableau à trier:
+    size_t j;
+    for(auto i = first; i != last ; i++){
+        //j est la position dans le nouveau tableau (ici output) de l'élément *i
+        j = countingTab.at(key(*i));
+        *(output + j) = *i ;
+        ++countingTab.at(key(*i));
+    }
+  }
+
+  /**
+   Tri par base d'entiers non signés sur 32 bits mis en oeuvre en appelant
+   4 fois le tri comptage en triant successivement par groupe de 8 bits.
+
+   https://en.wikipedia.org/wiki/Radix_sort
+
+   @param v vecteur à trier, modifié par cette fonction
+   */
+  void RadixSort(std::vector<unsigned int>& v)
+  {
+      std::vector<unsigned int> w(v.size());
+      const size_t MASK_SIZE = 0xff; //masque sur 8 bits
+      const size_t MAX_KEY = 0b11111111; //valeur maximale pour 8 bits
+      //On "trie comptage" 4 fois sur les groupes de 8 bits.
+      //La fonction lambda s'occupe de selectioner la bonne range de bits de l'unsigned int
+      for(size_t i = 0 ; i < 4 ; i++){
+        CountingSort(v.begin(),v.end(),w.begin(),[i,MASK_SIZE](const unsigned& val ){ return ((val >> (i * 8)) & MASK_SIZE);},MAX_KEY);
+        v = w;
+     }
+  }
 
 // Classe encapsulant un entier non signé. Elle ne met pas
 // en oeuvre les opérateurs de comparaison tels que <, >, ...
